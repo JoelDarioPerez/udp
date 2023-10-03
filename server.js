2af70d8445859d9ea521360ec0b9b337e18233c8
Joel Perez <perezjoeldario@gmail.com>
2023-10-03 17:38:42 -0300

    A

---
+++ server.js
@@ -0,0 +1,30 @@
+import dgram from "dgram";
+import dgram from "dgram";
+
+// Crear un servidor UDP
+const udpServer = dgram.createSocket("udp4");
+
+// Configurar el servidor para escuchar en un puerto específico
+const udpPort = 6002;
+
+udpServer.on("error", (err) => {
+  console.error(`Error en el servidor UDP: ${err}`);
+  udpServer.close();
+});
+
+udpServer.on("message", (message, rinfo) => {
+  // Cuando se recibe un paquete, se muestra por consola
+  console.log(
+    `Mensaje recibido desde ${rinfo.address}:${
+      rinfo.port
+    }: ${message.toString()}`
+  );
+});
+
+udpServer.on("listening", () => {
+  const address = udpServer.address();
+  console.log(`Servidor UDP escuchando en ${address.address}:${address.port}`);
+});
+
+// Iniciar el servidor UDP en el puerto especificado
+udpServer.bind(udpPort);
